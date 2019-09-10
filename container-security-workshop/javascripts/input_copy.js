5d0b6504085c63242fd28a2d05af367123e54b44
danidoo <danidoo@gmail.com>
2019-09-10 00:03:26 -0300

    updated container security workshop

--- container-security-workshop/javascripts/input_copy.js
+++ container-security-workshop/javascripts/input_copy.js
@@ -4,3 +4,10 @@ function copyval(el) {
         out[j].textContent = el.value;
     }
 }
+
+function copyval_replace(el, original, replacement) {
+    var out = document.getElementsByClassName(el.id);
+    for (j=0; j < out.length; j++) {
+        out[j].textContent = el.value.replace(original, replacement);
+    }
+}
